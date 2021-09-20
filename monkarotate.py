b85b8adabe8ccb345f33e5dd3cd49fba7c21e892
Abhishek Dalai <dharmakshetre5@gmail.com>
2021-09-20 10:17:22 +0530

    Update monkarotate.py

--- monkarotate.py
+++ monkarotate.py
@@ -2,8 +2,7 @@ t = int(raw_input())
 while t!=0:
     n,k = map(int, raw_input().split())
     a = map(int, raw_input().split())
-    index = n - (k%n) 
-    
+    index = n - (k%n)  
     for i in range(index,n):
      print a[i],
     for i in range(index):
